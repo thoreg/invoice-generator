71246bb6fccd99e2c0869a862ce5e5c411f4aea0
Thomas Rega <thomas.rega@momox.biz>
2014-12-02 20:42:22 +0100

    Add order as readonly inline

--- m13/sale/admin.py
+++ m13/sale/admin.py
@@ -3,19 +3,41 @@ from django.contrib import admin
 from sale.models import Address, Marketplace, Order
 
 
+class ReadOnlyTabularInline(admin.TabularInline):
+    extra = 0
+    can_delete = False
+    editable_fields = []
+    readonly_fields = []
+    exclude = []
+
+    def get_readonly_fields(self, request, obj=None):
+        return list(self.readonly_fields) + \
+            [field.name for field in self.model._meta.fields
+                if field.name not in self.editable_fields and field.name not in self.exclude]
+
+    def has_add_permission(self, request):
+        return False
+
+
 class MarketplaceAdmin(admin.ModelAdmin):
     list_display = ('name', 'vendor_id')
 
 
-class AddressInline(admin.TabularInline):
-    model = Address
+class OrderInline(ReadOnlyTabularInline):
+    model = Order
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city', 'street', 'postal_code', 'country_code')
+    inlines = [
+        OrderInline,
+    ]
 
 
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('marketplace_order_id',)
-    inlines = [
-        AddressInline,
-    ]
+
 
 admin.site.register(Marketplace, MarketplaceAdmin)
 admin.site.register(Order, OrderAdmin)
+admin.site.register(Address, AddressAdmin)

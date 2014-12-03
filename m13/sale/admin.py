b3320c33961a91163d35f221b6682116e5e8c4da
Thomas Rega <thomas.rega@momox.biz>
2014-12-04 00:57:37 +0100

    Invoice admin stuff

--- m13/sale/admin.py
+++ m13/sale/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from m13.sale.models import Address, Marketplace, Order
+from m13.sale.models import Address, Marketplace, Order, Invoice
 
 
 class ReadOnlyTabularInline(admin.TabularInline):
@@ -27,6 +27,10 @@ class OrderInline(ReadOnlyTabularInline):
     model = Order
 
 
+class InvoiceInline(ReadOnlyTabularInline):
+    model = Invoice
+
+
 class AddressAdmin(admin.ModelAdmin):
     list_display = ('name', 'city', 'street', 'postal_code', 'country_code')
     inlines = [
@@ -36,8 +40,16 @@ class AddressAdmin(admin.ModelAdmin):
 
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('marketplace_order_id',)
+    inlines = [
+        InvoiceInline,
+    ]
+
+
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ('invoice_number',)
 
 
 admin.site.register(Marketplace, MarketplaceAdmin)
 admin.site.register(Order, OrderAdmin)
 admin.site.register(Address, AddressAdmin)
+admin.site.register(Invoice, InvoiceAdmin)
--- m13/sale/models.py
+++ m13/sale/models.py
@@ -10,10 +10,16 @@ class Address(models.Model):
     postal_code = models.CharField(max_length=10)
     country_code = models.CharField(max_length=2)
 
+    def __str__(self):
+        return "{} {}".format(self.postal_code, self.name)
+
 
 class Marketplace(models.Model):
     name = models.CharField(max_length=20)
     vendor_id = models.CharField(max_length=14)
+
+    def __str__(self):
+        return self.name
 
 
 class Order(models.Model):
@@ -24,6 +30,9 @@ class Order(models.Model):
     email = models.EmailField()
     marketplace = models.ForeignKey(Marketplace)
     address = models.ForeignKey(Address)
+
+    def __str__(self):
+        return self.marketplace_order_id
 
     def get_total_sum(self):
         sum = 0
@@ -67,6 +76,9 @@ class Invoice(models.Model):
     number = models.IntegerField(null=True, blank=True)
     invoice_number = models.CharField(max_length=16, null=True, blank=True, db_index=True)
 
+    def __str__(self):
+        return self.invoice_number
+
     def save(self, *args, **kwargs):
         if not self.issue_time:
             self.issue_time = datetime.now()
@@ -89,6 +101,9 @@ class Product(models.Model):
     vendor_id = models.CharField(max_length="12")
     title = models.CharField(max_length=144)
 
+    def __str__(self):
+        self.title
+
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product)
@@ -99,6 +114,9 @@ class OrderItem(models.Model):
     sku = models.CharField(max_length=15)
     quantity_shipped = models.IntegerField()
     price = models.IntegerField()
+
+    def __str__(self):
+        return self.marketplace_orderitem_id
 
     def get_netto_price_as_string(self):
         return str(Decimal('{0:.2f}'.format(self.price / 119)))

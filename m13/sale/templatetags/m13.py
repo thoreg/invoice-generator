69d1bfd98015e77449895f1be4acbd45264123c1
Thomas Rega <thomas.rega@momox.biz>
2014-12-03 22:21:26 +0100

    Generate title which is used as file name when the document is printed

--- m13/sale/templatetags/m13.py
+++ m13/sale/templatetags/m13.py
@@ -9,3 +9,14 @@ def remove_m13(value):
     return value
 
 register.filter('remove_m13', remove_m13)
+
+
+def max_15_chars(value):
+    value = value.split()
+    filter(None, value)
+    if '-' in value:
+        value.remove('-')
+    return '-'.join(value)[:15]
+
+register.filter('remove_m13', remove_m13)
+register.filter('max_15_chars', max_15_chars)
